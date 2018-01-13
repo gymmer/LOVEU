393205bf696d1811cc52924309d26cfa937aaf2f
gymmer <511470840@qq.com>
2018-01-13 14:11:22 +0800

    V 3.1

---
+++ def_toolButton.cpp
@@ -0,0 +1,32 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+void MainWindow::on_refresh_time_toolButton_clicked()
+{
+    on_rfreshtime_triggered();
+}
+
+void MainWindow::on_set_time_toolButton_clicked()
+{
+    on_settime_triggered();
+}
+
+void MainWindow::on_refresh_memory_toolButton_clicked()
+{
+    on_rfreshmemory_triggered();
+}
+
+void MainWindow::on_set_memory_toolButton_clicked()
+{
+    on_setmemory_triggered();
+}
+
+void MainWindow::on_refresh_birthday_toolButton_clicked()
+{
+    on_rfreshbirthday_triggered();
+}
+
+void MainWindow::on_set_birthday_toolButton_clicked()
+{
+    on_setbirthday_triggered();
+}
